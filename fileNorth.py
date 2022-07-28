40c2bfbf15f581f70936dd2afbfc3cad229ed1c9
Emily <heyemmers@gmail.com>
2022-07-27 22:18:05 -0400

    add bedtime function

--- fileNorth.py
+++ fileNorth.py
@@ -7,3 +7,16 @@ C = A+B
 print(C)
 
 # What time is bed?
+def bedtime(tired_level):
+    if tired_level < 5:
+        return "Bed Time is 12:30am"
+    elif tired_level < 7:
+        return "Bed Time is 12:00am"
+    elif tired_level < 9:
+        return "Bed Time is 11:00pm"
+    else:
+        return "Bed Time is NOW!"
+
+tired_level = input("What is your tired level on a scale between 1-10?:")
+
+print(bedtime(tired_level))
